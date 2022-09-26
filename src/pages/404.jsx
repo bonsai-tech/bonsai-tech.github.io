e781f7091648b3e09e3f91aabbc069daeea718c3
Sara Moreno <sara.morenoj@gmail.com>
2022-09-26 20:35:02 +0200

    update

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,23 +4,26 @@ import { IntlProvider } from "react-intl";
 import AnimatedGrid from "../ui/components/AnimatedGrid";
 import logo from "../assets/images/logo.svg";
 import Link from "../ui/components/LinkLocalized";
+import AppWrapper from "../ui/layouts/AppWarpper";
 
 const NotFoundPage = () => {
   return (
     <IntlProvider>
-      <div className="not-found-page">
-        <AnimatedGrid />
-        <div className="content">
-          <Link to="/" className="logo">
-            <img className="logo" alt="Bonsai" src={logo} />
-          </Link>
-          <h1 className="alpha">Page not found</h1>
-          <p className="intro-text">
-            Sorry, we couldn’t find what you were looking for.
-          </p>
-          <ButtonLink to="/" label="Go home" />
+      <AppWrapper>
+        <div className="not-found-page">
+          <AnimatedGrid />
+          <div className="content">
+            <Link to="/" className="logo">
+              <img className="logo" alt="Bonsai" src={logo} />
+            </Link>
+            <h1 className="alpha">Page not found</h1>
+            <p className="intro-text">
+              Sorry, we couldn’t find what you were looking for.
+            </p>
+            <ButtonLink to="/" label="Go home" />
+          </div>
         </div>
-      </div>
+      </AppWrapper>
     </IntlProvider>
   );
 };
